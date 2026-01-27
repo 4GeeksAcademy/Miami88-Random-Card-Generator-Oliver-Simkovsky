4f2eb33e66eaec4aa033f08005bff159a86625b5
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-27 22:58:48 +0000

    starting functionality over. starting with just plain design

--- src/app.js
+++ src/app.js
@@ -6,44 +6,5 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-
-  // variables for button
-  let button = document.querySelector(".button");
-
-
-  // variables for card object
-  let element = document.querySelector(".obj");
-
-  let suitType = ["♦", "♥", "♠", "♣"];
   
-  let random = Math.floor(Math.random() * suitType.length);
-  let randomSuitType = suitType[random];
-  let deck = [];
-
-
-  let num = Math.floor(Math.random() * 13);
-
-  function createCard(randomSuitType, num, element) {
-    return {
-      suit: randomSuitType,
-      number: num,
-      elem: element
-    };
-  }
-
-  const first = createCard(randomSuitType, num, element);
-  deck.push(first);
-  for (let i = 0; i < deck.length; i++) {
-      console.log(deck[i]);
-    }
-
-  function buttonClick(e) {
-    const latestCard = createCard(randomSuitType, num, element);
-    deck.push(latestCard);
-    for (let i = 0; i < deck.length; i++) {
-      console.log(deck[i]);
-    }
-  }
-
-  button.addEventListener("click", buttonClick);
 };
