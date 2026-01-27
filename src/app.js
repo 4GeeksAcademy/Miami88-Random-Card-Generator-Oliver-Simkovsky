e66482f818a4bc8eb5abb7b48f12cdca81391095
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-01-27 23:31:59 +0000

    worked on functionality with test div

--- src/app.js
+++ src/app.js
@@ -6,5 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  
+  const container = document.querySelector("#container");
+  const button = this.document.querySelector("#btn");
+
+  function addButton(e){
+    const newDiv = document.createElement("div");
+    newDiv.className = "text-center";
+    newDiv.textContent = "hello";
+
+    container.appendChild(newDiv);
+  }
+
+  button.addEventListener("click", addButton);
 };
